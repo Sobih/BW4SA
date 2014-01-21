53ffa7fb178eef6e17bbb40ae00e1ac10d0d607c
Vaakapallo <Lassi.Vapaakallio@cs.helsinki.fi>
2014-01-21 17:19:36 +0200

    Preliminary count_substrings

---
+++ count_substrings.h
@@ -0,0 +1,13 @@
+/*
+ * count_substrings.h
+ *
+ *  Created on: 21.1.2014
+ *      Author: lvapaaka
+ */
+
+#ifndef COUNT_SUBSTRINGS_H_
+#define COUNT_SUBSTRINGS_H_
+
+
+
+#endif /* COUNT_SUBSTRINGS_H_ */
