690dbfc332c6d31c2482da0a0edc2037bfa2e0b3
Max Sandberg <sandberg.max@gmail.com>
2014-01-21 17:15:48 +0200

    Initial commit of tests for bit_array

---
+++ tests/array/check_bit_array.c
@@ -0,0 +1,20 @@
+/*
+ * check_bit_array.c
+ *
+ *  Created on: 21.1.2014
+ *      Author: Max Sandberg
+ */
+
+#include <check.h>
+#include "../../include/bit_array.h"
+
+START_TEST (test_alphabet_mapping) {
+	char* string = "asufgvakjsvfakuyftkjbvqygkjavjggjfaysggaaysgfysuuuysgfsuaguygsaygfugaysfguasygfu";
+
+	char_mapping* mapping = map_alphabet(string);
+	unsigned int length = mapping->bit_value;
+
+	for (int i = 0; i < length + 1; ++i)
+		sprintf("%c\t%d\n", mapping[i].character, mapping[i].bit_value);
+}
+END_TEST
