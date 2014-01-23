b2ff3bf2e59681e59f94b960d64dc38b33bfe10e
Vaakapallo <Lassi.Vapaakallio@cs.helsinki.fi>
2014-01-23 18:44:56 +0200

    header added to dbwt.h

--- src/main.c
+++ src/main.c
@@ -5,8 +5,22 @@
  *      Author: lvapaaka
  */
 
+#include "dbwt.h"
+#include <stdio.h>
+#include <string.h>
+
 int main(void) {
 	printf("HELLO, WORLD!");
+	FILE *f;
+	char c[50];
+	if (!(f = fopen("test.txt", "a"))) {
+		return 1;
+	}
+	fgets(c, 50, "abracadabra");
+	fputs(c, f);
+	fclose(f);
+
+	bwt("test.txt");
 
 	return 0;
 }
