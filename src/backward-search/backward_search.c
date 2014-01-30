24c53fb11ffbe8bb6904edfb99610c0267bbecc2
lassi.vapaakallio@cs.helsinki.fi <Lassi.Vapaakallio@cs.helsinki.fi>
2014-01-30 16:51:05 +0200

    commented rank

--- src/backward-search/backward_search.c
+++ src/backward-search/backward_search.c
@@ -5,6 +5,15 @@
  *      Author: lvapaaka
  */
 
+/**
+ * @brief Counts the number of occurrences of the given character in the given string before the given index.
+ *
+ * @param index search for c from 0 to (index -1)
+ * @param c character to be counted
+ * @param string the string searched
+ * @author Lassi Vapaakallio, Neo Lehtola
+ * @return how many occurrences of the character c
+ */
 int rank(const int index, const char c, const char* string){
 	int i, count = 0;
 	if(index <= 0){
