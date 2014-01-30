dca377f17d182ea4df9563b9e52fed736f4c90b5
topipaavilainen@gmail.com <topatopa@dat-8.cs.helsinki.fi>
2014-01-30 18:56:54 +0200

    backward search implemented, tested and commented

--- src/backward-search/backward_search.c
+++ src/backward-search/backward_search.c
@@ -1,3 +1,8 @@
+
+#include <string.h>
+#include <stdlib.h>
+#include "../../include/c_array.h"
+#include "../../include/backward_search.h"
 /*
  * backward_search.c
  *
@@ -5,15 +10,6 @@
  *      Author: lvapaaka
  */
 
-/**
- * @brief Counts the number of occurrences of the given character in the given string before the given index.
- *
- * @param index search for c from 0 to (index -1)
- * @param c character to be counted
- * @param string the string searched
- * @author Lassi Vapaakallio, Neo Lehtola
- * @return how many occurrences of the character c
- */
 int rank(const int index, const char c, const char* string){
 	int i, count = 0;
 	if(index <= 0){
@@ -27,3 +23,37 @@ int rank(const int index, const char c, const char* string){
 	}
 	return count;
 }
+
+int get_char_index(int* c_array, char* alphabet, char c);
+
+Interval* backward_search(const char* bwt, const char* string)
+{
+	int i = 0;
+	int j = strlen(bwt)-1;
+	char* alphabet = get_alphabet(bwt);
+	int* c_array = create_c_array(bwt);
+	int k;
+	for(k = strlen(string)-1; k>=0; k--){
+		if(j<i){
+			return NULL;
+		}
+		i = get_char_index(c_array, alphabet, string[k])+rank(i, string[k], bwt);
+		j = get_char_index(c_array, alphabet, string[k])+rank(j+1, string[k], bwt)-1;
+	}
+	Interval* new_interval = malloc(sizeof(Interval));
+	new_interval->i = i;
+	new_interval->j = j;
+	return new_interval;		
+}
+
+int get_char_index(int* c_array, char* alphabet, char c)
+{
+	int i;
+	for(i=0;i<strlen(alphabet);i++){
+		if(alphabet[i] == c){
+			return c_array[i];
+		}
+	}
+	return -1;
+}
+	
--- tests/backward-search/check_backward_search.c
+++ tests/backward-search/check_backward_search.c
@@ -5,7 +5,7 @@
  *      Author: lvapaaka
  */
 
-#include "backward_search.h"
+#include "../../include/backward_search.h"
 #include <check.h>
 #include <stdio.h>
 
@@ -37,13 +37,63 @@ START_TEST(test_count_some_a_letters)
 }
 END_TEST
 
+START_TEST(test_search_simple)
+{
+	Interval* interval = backward_search("ard$rcaaaabb", "abra");
+	ck_assert_int_eq(2, interval->i);
+	ck_assert_int_eq(3, interval->j);
+}
+END_TEST
+
+START_TEST(test_search_simple2)
+{
+	Interval* interval = backward_search("ard$rcaaaabb", "bra");
+	ck_assert_int_eq(6, interval->i);
+	ck_assert_int_eq(7, interval->j);
+}
+END_TEST
+
+START_TEST(test_search_simple3)
+{
+	Interval* interval = backward_search("ard$rcaaaabb", "abracadabra");
+	ck_assert_int_eq(3, interval->i);
+	ck_assert_int_eq(3, interval->j);
+}
+END_TEST
+
+START_TEST(test_search_when_not_found)
+{
+	
+	Interval* interval = backward_search("ard$rcaaaabb", "nakki");
+	fail_unless(interval == NULL);
+}
+END_TEST
+
+START_TEST(test_search_different_word)
+{
+	
+	Interval* interval = backward_search("ipssm$pissii", "iss");
+	ck_assert_int_eq(3, interval->i);
+	ck_assert_int_eq(4, interval->j);
+}
+END_TEST
+
+TCase * create_rank_test_case(void){
+	TCase * tc_rank = tcase_create("rank_test");
+	tcase_add_test(tc_rank, test_count_zero_letters);
+	tcase_add_test(tc_rank, test_count_one_letter);
+	tcase_add_test(tc_rank, test_count_all_a_letters);
+	tcase_add_test(tc_rank, test_count_some_a_letters);
+
+	return tc_rank;
+}
 TCase * create_backward_search_test_case(void){
 	TCase * tc_backward_search = tcase_create("backward_search_test");
-	tcase_add_test(tc_backward_search, test_count_zero_letters);
-	tcase_add_test(tc_backward_search, test_count_one_letter);
-	tcase_add_test(tc_backward_search, test_count_all_a_letters);
-	tcase_add_test(tc_backward_search, test_count_some_a_letters);
-
+	tcase_add_test(tc_backward_search, test_search_simple);
+	tcase_add_test(tc_backward_search, test_search_simple2);
+	tcase_add_test(tc_backward_search, test_search_simple3);
+	tcase_add_test(tc_backward_search, test_search_when_not_found);
+	tcase_add_test(tc_backward_search, test_search_different_word);
 
 	return tc_backward_search;
 }
@@ -52,8 +102,9 @@ Suite * test_suite(void)
 {
 	Suite *s = suite_create("testi");
 	TCase *tc_backward_search = create_backward_search_test_case();
+	TCase *tc_rank = create_rank_test_case();
 	suite_add_tcase(s, tc_backward_search);
-
+	suite_add_tcase(s, tc_rank);
 	return s;
 }
 
