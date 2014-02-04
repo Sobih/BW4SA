e8938f5575fd5f96b310af39f79e05cb29469029
Max Sandberg <sandberg.max@gmail.com>
2014-02-04 18:50:01 +0200

    First commit of the wavelet tree structure

---
+++ include/wavelet_tree.h
@@ -0,0 +1,22 @@
+/*
+ * wavelet_tree.h
+ *
+ *  Created on: 4.2.2014
+ *      Author: maxsandb
+ */
+
+#ifndef WAVELET_TREE_H_
+#define WAVELET_TREE_H_
+
+struct wavelet_node {
+	char* string;
+	unsigned int* bit_vector;	//TODO: MODIFY THIS!!!!!!!!!!!
+	char* alphabet;
+	unsigned int alphabet_length;
+	struct wavelet_node* parent;
+	struct wavelet_node* children;
+} wavelet_node;
+
+wavelet_node* create_wavelet_tree(char* string);
+
+#endif /* WAVELET_TREE_H_ */
---
+++ src/wavelet-tree/wavelet_tree.c
@@ -0,0 +1,103 @@
+/*
+ * wavelet_tree.c
+ *
+ *  Created on: 4.2.2014
+ *      Author: Max Sandberg
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "../../include/utils.h"
+#include "../../include/wavelet_tree.h"
+
+wavelet_node* init_node(struct wavelet_node* node, const char* parent_string, const char* alphabet,
+		unsigned int alphabet_length) {
+	node->alphabet = (char*) alphabet;
+	node->alphabet_length = alphabet_length;
+	node->string = determine_substring(parent_string, node->alphabet, node->alphabet_length);
+	node->bit_vector = create_bit_vector(node->string, node->alphabet);
+
+	return node;
+}
+
+unsigned int* create_bit_vector(const char* string, const char* alphabet) {
+	//TODO: Implement this once a bit vector representation has been determined
+	return NULL;
+}
+
+char* determine_substring(const char* string, const char* alphabet, unsigned int alphabet_size) {
+	int size = 0, chars_left = 10;
+	char* substring = calloc(chars_left, sizeof(char));
+
+	for (int i = 0; i < strlen(string); ++i)
+		//if current character in string can be found in alphabet, append to substring
+		if (binary_search(alphabet, string + i, 0, alphabet_size, sizeof(char)) >= 0) {
+
+			if (chars_left == 0) {
+				//expand array
+				chars_left = 10;
+
+				if ((substring = realloc(substring, size + chars_left)) == 0)
+					//realloc failed, return NULL
+					return 0;
+			}
+
+			//add to substring
+			chars_left--;
+			substring[size] = *(string + i);
+			size++;
+		}
+
+	//add null-marker
+	if (chars_left == 0)
+		//expand alphabet array
+		if ((substring = realloc(substring, size + 1)) == 0)
+			//realloc failed, return NULL
+			return 0;
+
+	//add to alphabet
+	substring[size] = 0;
+
+	return substring;
+}
+
+wavelet_node* create_children(struct wavelet_node* node) {
+	//alphabet can still be split
+	if (node->alphabet_length > 2) {
+		node->children = calloc(2, sizeof(wavelet_node));
+		struct wavelet_node* left_child = node->children[0], right_child = node->children[1];
+		unsigned int left_length = ceil(node->alphabet_length / 2),
+				right_length = node->alphabet_length - left_length;
+
+		//recursively create children of left child
+		left_child = init_node(left_child, node->string, node->alphabet, left_length);
+		create_children(left_child);
+
+		//recursively create children of right child
+		right_child = init_node(right_child, node->string, node->alphabet + left_length, right_length);
+		create_children(right_child);
+	}
+
+	//alphabet small enough = no children
+	else
+		node->children = NULL;
+
+	return node;
+}
+
+wavelet_node* create_wavelet_tree(char* string) {
+	//allocate the root node
+	struct wavelet_node* root = calloc(1, sizeof(wavelet_node));
+
+	//determine the alphabet and its length
+	char* alphabet = determine_alphabet(string);
+	unsigned int alphabet_length = strlen(alphabet);
+	alphabet = quick_sort(alphabet, alphabet_length, sizeof(char));
+
+	//init root and populate tree
+	root = init_node(root, string, alphabet, alphabet_length);
+	root = create_children(root);
+
+	return root;
+}
