e238bf3752bc8ce232eaac1a2863e19cd242a38e
Max Sandberg <sandberg.max@gmail.com>
2014-02-06 18:03:56 +0200

    Modified build script to test the new bit vector

--- os/array_tests.sh
+++ os/array_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -std=c99 -o src/array/bit_array.o src/array/bit_array.c tests/array/check_bit_array.c src/utils.c `pkg-config --cflags --libs check` -lm
+gcc -std=c99 -o src/array/bit_vector.o src/array/bit_vector.c tests/array/check_bit_vector.c src/utils.c `pkg-config --cflags --libs check` -lm
 
-./src/array/bit_array.o
+./src/array/bit_vector.o
