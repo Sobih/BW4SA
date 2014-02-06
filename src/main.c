36a655b7c92c76c349fc8e48187c60a2becf0183
lassi.vapaakallio@cs.helsinki.fi <Lassi.Vapaakallio@cs.helsinki.fi>
2014-02-06 16:59:43 +0200

    added relevant files for a better DBWT library and tested it in main.

--- src/main.c
+++ src/main.c
@@ -7,12 +7,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-
+#include "dbwt.h"
 
 int main(void) {
+	uchar *c = calloc(100, sizeof(uchar));
+	c = "abracadabraaaaaaaaa";
+	long n = strlen(c);
+
+	printf("%li\n",n);
+
+	unsigned int *last = &c + 500;
+
+	printf("%d\n", last);
+
+	uchar *d = calloc(100,sizeof(uchar));
+
+	d = dbwt_bwt(c,n,last,0);
+
+	printf("%s\n%s\n", c,d);
+
+	printf("%d\n", last);
+
+	free(d);
 
 	return 0;
 }
