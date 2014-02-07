865462e7e482dbd4f3e15103c69196b3b5a9820d
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-02-07 15:39:56 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,16 +6,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "s_to_bwt.h"
 
 int main(void) {
-
-	unsigned char *c = "vaakapallo";
-
-	unsigned char *d = calloc(strlen(c) + 1, sizeof(unsigned char));
-	d = s_to_BWT(c);
-
-	printf("%s\n", d);
 
 	return 0;
 }
