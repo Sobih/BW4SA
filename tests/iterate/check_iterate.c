7bb0600861c5c565cabdbcd04a7fd47fd6bb7852
topipaavilainen@gmail.com <topatopa@dat-6.cs.helsinki.fi>
2014-02-11 16:21:29 +0200

    query for interval from runs-vector added

--- tests/iterate/check_iterate.c
+++ tests/iterate/check_iterate.c
@@ -3,6 +3,7 @@
 #include "../../include/iterate.h"
 #include "../../src/iterate/substring_stack.h"
 #include "../../include/bit_vector.h"
+#include "../../include/backward_search.h"
 #include <stdlib.h>
 
 START_TEST(simple_runs_test)
@@ -42,10 +43,41 @@ START_TEST(another_simple_test)
 }
 END_TEST
 
+START_TEST(test_interval_query)
+{
+	char* string = "ABRACADABRA";
+	bit_vector* runs = create_runs_vector(string);
+	Interval* test1 = malloc(sizeof(Interval));
+	test1->i = 6;
+	test1->j = 7;
+	Interval* test2 = malloc(sizeof(Interval));
+	test2->i = 1;
+	test2->j = 3;
+	ck_assert_int_eq(0, is_reverse_interval_right_maximal(runs, test1));
+	ck_assert_int_eq(1, is_reverse_interval_right_maximal(runs, test2));
+}
+END_TEST
+
+START_TEST(test_wrong_intervals)
+{
+	char* string = "ABRACADABRA";
+	bit_vector* runs = create_runs_vector(string);
+	Interval* test1 = malloc(sizeof(Interval));
+	test1->i = 6;
+	test1->j = 4;
+	Interval* test2 = malloc(sizeof(Interval));
+	test2->i = 6;
+	test2->j = 6;
+	ck_assert_int_eq(0, is_reverse_interval_right_maximal(runs, test1));
+	ck_assert_int_eq(0, is_reverse_interval_right_maximal(runs, test2));
+}
+END_TEST
 TCase * create_runs_vec_test_case(void){
 	TCase * tc_runs = tcase_create("runs_vec_test");
 	tcase_add_test(tc_runs, simple_runs_test);
 	tcase_add_test(tc_runs, another_simple_test);
+	tcase_add_test(tc_runs, test_interval_query);
+	tcase_add_test(tc_runs, test_wrong_intervals);
 	return tc_runs;
 }
 
