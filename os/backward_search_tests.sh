87bf8bde36d54fcde118c99a8475d43774a6841e
Max Sandberg <sandberg.max@gmail.com>
2014-02-27 16:27:40 +0200

    Added bit_vector.c and wavelet_tree.c to script

--- os/backward_search_tests.sh
+++ os/backward_search_tests.sh
@@ -1,3 +1,3 @@
-gcc -std=c99 -o src/backward-search/backward_search.o src/backward-search/backward_search.c tests/backward-search/check_backward_search.c src/backward-search/c_array.c src/utils.c `pkg-config --cflags --libs check`
+gcc -std=c99 -o src/backward-search/backward_search.o src/array/bit_vector.c src/wavelet_tree/wavelet_tree.c src/backward-search/backward_search.c tests/backward-search/check_backward_search.c src/backward-search/c_array.c src/utils.c `pkg-config --cflags --libs check` -lm
 
 src/backward-search/backward_search.o
