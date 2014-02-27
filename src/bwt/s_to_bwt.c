2d1a3878c7c00133dd144d9c9a7fba0f523dcb9a
Max Sandberg <sandberg.max@gmail.com>
2014-02-27 16:28:23 +0200

    Made s_to_bwt() return a wavelet tree instead of string

--- src/bwt/s_to_bwt.c
+++ src/bwt/s_to_bwt.c
@@ -8,16 +8,16 @@
 #include "dbwt.h"
 #include <stdlib.h>
 #include <string.h>
+#include "s_to_bwt.h"
 
-uchar* s_to_BWT(uchar *string) {
-	unsigned int * _last;
+wavelet_node* s_to_BWT (uchar* string) {
 	unsigned int last;
 	long n = strlen(string);
-	uchar *d = calloc(n + 2, sizeof(uchar));
+	uchar* d = malloc(n + 2 * sizeof(uchar));
 	d = dbwt_bwt(string, n, &last, 0);
 	d[last] = '$';
 	d[n + 1] = 0;
-	//(*_last) = last;
-	return d;
+
+	return create_wavelet_tree(d);
 }
 
--- src/bwt/s_to_bwt.h
+++ src/bwt/s_to_bwt.h
@@ -11,12 +11,14 @@
 typedef unsigned char uchar;
 #endif
 
+#include "../../include/wavelet_tree.h"
+
 /**
  * Performs Burrows-Wheeler transform on the given string.
  *
  * @param string the string on which the Burrows-Wheeler transform is applied to.
  * @return the BWT of the string
  */
-uchar* s_to_BWT(uchar *string);
+wavelet_node* s_to_BWT(uchar *string);
 
 #endif /* S_TO_BWT_H_ */
