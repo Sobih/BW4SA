6ac38804b74b4d17638c830bf3f18863e157a038
Max Sandberg <sandberg.max@gmail.com>
2014-02-27 16:23:58 +0200

    Removed unnecessary methods and comments

--- include/backward_search.h
+++ include/backward_search.h
@@ -2,16 +2,16 @@
 #ifndef BACKWARD_SEARCH_H_
 #define BACKWARD_SEARCH_H_
 
+#include "wavelet_tree.h"
+
 /**
 * Struct for intervals in bwt
 **/
-typedef struct inter
+typedef struct interval
 {
 	int i;
 	int j;
 } interval;
-
-struct wavelet_node;
 
 /**
 * @brief function that finds interval of given string from given bwt
@@ -23,7 +23,7 @@ struct wavelet_node;
 * @return interval of searched string
 *
 **/
-interval* backward_search(const struct wavelet_node* bwt, const struct wavelet_node* string);
+interval* backward_search(const wavelet_node* bwt, const struct wavelet_node* string);
 
 /**
  * @brief finds the interval of a given letter in an interval.
@@ -37,6 +37,6 @@ interval* backward_search(const struct wavelet_node* bwt, const struct wavelet_n
  * @return interval of searched left extension
  *
  */
-interval* backward_search_interval(const struct wavelet_node* bwt, const interval* interval, char c);
+interval* backward_search_interval(const wavelet_node* bwt, const interval* interval, char c);
 
 #endif /* BACKWARD_SEARCH_H_ */
--- src/backward-search/backward_search.c
+++ src/backward-search/backward_search.c
@@ -6,21 +6,25 @@
 #include "../../include/backward_search.h"
 #include "../../include/wavelet_tree.h"
 
-interval* backward_search(const wavelet_node* bwt, const wavelet_node* string)
-{
+interval* backward_search(const wavelet_node* bwt, const wavelet_node* string) {
 	bit_vector* bwt_vec = bwt->vector, *str_vec = string->vector;
-	int i = 0, j = bwt_vec->length * 32 - bwt_vec->filler_bits - 1, k, index;
+	int i = 0, j = bwt_vec->get_length(bwt_vec) - 1, k, index;
+	unsigned int alphabet_length = bwt->alphabet_length - 1;
 	char* alphabet = bwt->alphabet, current;
-	int* c_array = create_c_array(bwt);
+	unsigned int* c_array = create_c_array(bwt);
 
-	for(k = str_vec->length * 32 - str_vec->filler_bits - 1; k >= 0; k--) {
+	for(k = str_vec->get_length(str_vec) - 1; k >= 0; k--) {
 		if(j < i)
 			return NULL;
 
 		current = string->char_at(string, k);
-		index = binary_search(alphabet, &current, 0, string->alphabet_length - 1, sizeof(char));
-		i = index + bwt->rank(bwt, current, i);
-		j = index + bwt->rank(bwt, current, j) - 1;
+		index = binary_search(alphabet, &current, 0, alphabet_length, sizeof(char));
+
+		if (index < 0)
+			return NULL;
+
+		i = c_array[index] + bwt->rank(bwt, current, i - 1);
+		j = c_array[index] + bwt->rank(bwt, current, j) - 1;
 	}
 
 	interval* new_interval = malloc(sizeof(interval));
@@ -35,12 +39,12 @@ interval* backward_search_interval(const wavelet_node* bwt, const interval* inte
 	char* alphabet = bwt->alphabet;
 
 	int index = binary_search(alphabet, &c, 0, bwt->alphabet_length - 1, sizeof(char));
-	int i = index + bwt->rank(bwt, c, interval->i);
+	int i = index + bwt->rank(bwt, c, interval->i - 1);
 	int j = index + bwt->rank(bwt, c, interval->j) - 1;
 
 	if (i > j) return NULL;
 
-	struct inter* new_interval = calloc(1, sizeof(struct inter));
+	struct interval* new_interval = malloc(sizeof(struct interval));
 	new_interval->i = i;
 	new_interval->j = j;
 
