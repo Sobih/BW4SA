eea55f6365a92697ab59d0b686f9ad7b2f820214
topipaavilainen@gmail.com <topatopa@dat-8.cs.helsinki.fi>
2014-03-18 15:09:00 +0200

    conflict fixed

--- src/main.c
+++ src/main.c
@@ -6,20 +6,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 #include "../include/bwt_to_s.h"
 #include "../include/draw_tree.h"
 #include "../include/iterate.h"
 #include "iterate/print_node.h"
-
-int main(void) {
-	char* test = "abracadabra";
-	draw_suffix_tree(test);
-=======
 #include "ui/ui.h"
 
 int main(void) {
 	ui();
->>>>>>> 3f0d34f8ce1c58dace5cdb633ff9807f9586c147
 	return 0;
 }
