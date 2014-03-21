275a0f97fa584670eb87fb1960bf1d886a29d7a0
pklehtol@cs.helsinki.fi <pklehtol@dat-7.cs.helsinki.fi>
2014-03-21 17:00:36 +0200

    tripletsorter tests

--- src/iterate/triplet_sorter.c
+++ src/iterate/triplet_sorter.c
@@ -4,11 +4,13 @@
 #include <stdio.h>
 // swap function in utils.h
 
-int compare_ints(int a, int b) {
-	return a - b;
+int compare_ints(int* a, int* b) {
+	return *a - *b;
 }
 
 int compare_triplets_pos1(triplet* first, triplet* second) {
+
+	printf("first: %d second: %d\n", first->pos1, second->pos1);
 	return first->pos1 - second->pos1;
 }
 
@@ -30,41 +32,44 @@ void compare_quicksort(void* arr, unsigned int entry_size, unsigned int a, unsig
 	if (a >= b)
 		return;
 
+	//printf("TESTING\n");
 	void* key = (arr + (a * entry_size));
 	int i = a + 1, j = b;
-
+	
 	while (i < j) {
+		
 		//int w = compare((arr + (j * entry_size)), key);
 		//printf("w: %d\n", w);
 		while (i < j && compare((arr + (j * entry_size)), key) >= 0) {
 			j--;
-			printf("j: %d\n", j);
+			//printf("j: %d\n", j);
 		}
 			
 
 		while (i < j && compare((arr + (i * entry_size)), key) <= 0) {
-			//printf("TESTING\n");	
+				
 			i++;
 			//printf("i: %d\n", i);
 		}
 			
 		//printf("i: %d j: %d\n", i, j);
-		if (i < j) {			
+		if (i < j) {	
+			//printf("SWAPPING");		
 			swap(arr, entry_size, i, j);
 		}
-			
-		//int x = compare((arr + (a * entry_size)), (arr + (i * entry_size)));
-		//printf("x: %d\n", x);
-		if (compare((arr + (a * entry_size)), (arr + (i * entry_size))) > 0) {
 
-			swap(arr, entry_size, a, i);
-			compare_quicksort(arr, entry_size, a, i-1, compare);
-			compare_quicksort(arr, entry_size, i + 1, b, compare);
-		} else {
-			compare_quicksort (arr, entry_size, a+1, b, compare);
-		}
 	}
 
+	//int x = compare((arr + (a * entry_size)), (arr + (i * entry_size)));
+	//printf("x: %d\n", x);
+	if (compare((arr + (a * entry_size)), (arr + (i * entry_size))) > 0) {
+
+		swap(arr, entry_size, a, i);
+		compare_quicksort(arr, entry_size, a, i-1, compare);
+		compare_quicksort(arr, entry_size, i + 1, b, compare);
+	} else {
+		compare_quicksort (arr, entry_size, a+1, b, compare);
+	}
 }
 
 void compare_quick_sort(void* arr, unsigned int arr_size, unsigned int entry_size, int (*compare)(void* first, void* second)) {
--- tests/iterate/check_triplet_sorter.c
+++ tests/iterate/check_triplet_sorter.c
@@ -6,45 +6,56 @@
 #include "../../include/utils.h"
 #include "../../src/iterate/triplet_sorter.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 
-START_TEST(int_array)
+/*START_TEST(int_array)
 {
-	int array[5] = {5, 4, 3, 2, 1};
+	int array[5] = {5, 0, -3, 6, 1};
 
 	compare_quick_sort(array, 5, sizeof(int), &compare_ints);
 	
-	ck_assert_int_eq(0, array[0]);
+	ck_assert_int_eq(-3, array[0]);
+	ck_assert_int_eq(0, array[1]);
+	ck_assert_int_eq(1, array[2]);
+	ck_assert_int_eq(5, array[3]);
+	ck_assert_int_eq(6, array[4]);
+	
 
-} END_TEST
+} END_TEST*/
 
-/*START_TEST(test_pos1_1)
+START_TEST(test_pos1_1)
 {
 
 	triplet* first = &((triplet) {.pos1 = 5, .pos2 = 3, .length = 3});
-	triplet* second = &((triplet) {.pos1 = 0, .pos2 = 2, .length = 3});
+	triplet* second = &((triplet) {.pos1 = 1, .pos2 = 2, .length = 3});
 	triplet* third = &((triplet) {.pos1 = 3, .pos2 = 5, .length = 2});
+	
 	
 	triplet* array[3];
 	
 	array[0] = first;
 	array[1] = second;
 	array[2] = third;
+	//printf("TESTING2\n");
+	compare_quick_sort(*array, 3, sizeof(triplet*), &compare_triplets_pos1);
 	
-	compare_quick_sort(array, 3, sizeof(triplet*), &compare_triplets_pos1);
-	
+
 	triplet* a = (triplet*)array[0];
-	ck_assert_int_eq(0, a->pos1);
+	printf("first: %d\n", a->pos1);
+	ck_assert_int_eq(1, a->pos1);
 	//ck_assert_int_eq(2, a->pos2);
 	a = (triplet*)array[1];
+	printf("second: %d\n", a->pos1);
 	ck_assert_int_eq(3, a->pos1);
 	//ck_assert_int_eq(3, a->pos2);
 	a = (triplet*)array[2];
+	printf("third: %d\n", a->pos1);
 	ck_assert_int_eq(5, a->pos1); 
 	//ck_assert_int_eq(5, a->pos2);
 
 
-typedef struct triplet_struct {
+/*typedef struct triplet_struct {
 	unsigned int pos1;
 	unsigned int pos2;
 	unsigned int length;
@@ -54,13 +65,13 @@ typedef struct triplet_struct {
 //void compare_quick_sort(void* arr, unsigned int arr_size, unsigned int entry_size, int (*compare)(void* first, void* second))
 
 
-/*} END_TEST*/
+} END_TEST
 
 
 TCase * create_triplet_sorter_test_case(void) {
 	TCase * tc_stack = tcase_create("triplet_sorter_test");
-	tcase_add_test(tc_stack, int_array);
-	//tcase_add_test(tc_stack, test_pos1_1);
+	//tcase_add_test(tc_stack, int_array);
+	tcase_add_test(tc_stack, test_pos1_1);
 	return tc_stack;
 }
 
