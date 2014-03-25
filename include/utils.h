6f26c35166b6ab7d7c8b2d2dc20606845e405f2b
Max Sandberg <sandberg.max@gmail.com>
2014-03-25 17:20:32 +0200

    Made indices (max & min) signed ints and added a < 0 check

--- include/utils.h
+++ include/utils.h
@@ -81,7 +81,7 @@ char* determine_alphabet(const char* string);
  * @bug		No known bugs.
  */
 int binary_search(const void* arr, const void* key, unsigned int entry_size,
-		unsigned int max, unsigned int min);
+		int max, int min);
 
 /**
  * @brief	Prints the bits of an unsigned int.
