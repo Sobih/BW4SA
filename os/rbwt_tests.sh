3d6b62b3766f5759a4cad429092b153d94b36fe7
Max Sandberg <sandberg.max@gmail.com>
2014-03-27 15:43:24 +0200

    Added utils.c, wavelet_tree.c and bit_vector.c to build script

--- os/rbwt_tests.sh
+++ os/rbwt_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -std=c99 -o src/iterate/rbwt.o src/iterate/rbwt.c src/bwt/s_to_bwt.c -I src/bwt -I libs/DBWT/ libs/DBWT/*.c tests/iterate/check_rbwt.c `pkg-config --cflags --libs check`
+gcc -std=c99 -o src/iterate/rbwt.o src/iterate/rbwt.c src/bwt/s_to_bwt.c -I src/bwt -I libs/DBWT/ libs/DBWT/*.c tests/iterate/check_rbwt.c src/wavelet_tree/wavelet_tree.c src/array/bit_vector.c src/utils.c `pkg-config --cflags --libs check` -lm
 
 ./src/iterate/rbwt.o
