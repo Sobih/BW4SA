2e5fd3cb3a7041f6d696751385fdcb8bc5436c86
lassi.vapaakallio@cs.helsinki.fi <Lassi.Vapaakallio@cs.helsinki.fi>
2014-03-28 16:56:59 +0200

    double iterate bugs fixed. Maximal repeats now work with mapping as well.

--- src/iterate/iterate.c
+++ src/iterate/iterate.c
@@ -36,12 +36,16 @@ interval* update_reverse_interval(interval* inter, interval* normal,
 	if (target == 0)
 		target = malloc(sizeof(interval));
 
-	int char_index = c_array[binary_search(alphabet, &c, sizeof(char),
-			alphabet_length, 0)];
+	int index_in_c_array = binary_search(alphabet, &c, sizeof(char),
+			alphabet_length, 0);
+	int char_index = c_array[index_in_c_array];
 	target->i = inter->i + char_index;
 
 	//length of the reverse interval is same as length of the normal interval
-	target->j = target->i + (normal->j - normal->i);
+	int normal_j = normal->j;
+	int normal_i = normal->i;
+	int new_j = target->i + (normal_j - normal_i);
+	target->j = new_j;
 	return target;
 }
 
@@ -58,8 +62,11 @@ substring* create_substring(interval* normal, interval* reverse, int length,
 	if (target == 0)
 		target = malloc(sizeof(substring));
 
-	target->normal.i = normal->i;
-	target->normal.j = normal->j;
+	int normal_i = normal->i;
+	int normal_j = normal->j;
+
+	target->normal.i = normal_i;
+	target->normal.j = normal_j;
 
 	target->reverse.i = reverse->i;
 	target->reverse.j = reverse->j;
@@ -209,7 +216,7 @@ void double_iterate(char* string1, char* string2,
 		void (*callback)(substring* substr1, substring* substr2)) {
 
 	wavelet_tree* bwt1 = s_to_BWT(string1), *rev_bwt1 = reverse_bwt(string1);
-	wavelet_tree* bwt2 = s_to_BWT(string1), *rev_bwt2 = reverse_bwt(string1);
+	wavelet_tree* bwt2 = s_to_BWT(string2), *rev_bwt2 = reverse_bwt(string2);
 	bit_vector* reverse_runs1 = create_runs_vector(rev_bwt1, 0);
 	bit_vector* reverse_runs2 = create_runs_vector(rev_bwt2, 0);
 	substring_stack* stack1 = create_stack(10);
@@ -248,11 +255,16 @@ void double_iterate(char* string1, char* string2,
 			reverse2, 0, 0);
 
 	while (1) {
-		if (substring1 == NULL)
-			break;
 
-		if (substring2 == NULL)
+		if (substring1 == NULL){
+			printf("Substring 1 was null");
 			break;
+		}
+
+		if (substring2 == NULL){
+			printf("Substring 2 was null");
+			break;
+		}
 
 		//if size of the interval is 1, it cannot be a right-maximal string
 		//if(substring->normal->i == substring->normal->j) continue;
@@ -265,29 +277,38 @@ void double_iterate(char* string1, char* string2,
 		c_array1 = create_c_array(bwt1, &substring1->normal, 0, 0, c_array1);
 		c_array2 = create_c_array(bwt2, &substring2->normal, 0, 0, c_array2);
 
-		char alphabets[alpha_data1->length + alpha_data2->length];
+
+
+		char* alphabets = malloc (sizeof(char) * (alpha_data1->length + alpha_data2->length + 1));
 		strcpy(alphabets, alpha_data1->alphabet);
 		strcat(alphabets, alpha_data2->alphabet);
+
+		printf("Alphabets concatenated: %s\n", alphabets);
 
 		common_alphabet = determine_alphabet(alphabets);
 		int common_alphabet_length = strlen(common_alphabet);
 
 		for (i = 0; i < common_alphabet_length; i++) {
-			interval* normal1 = backward_search_interval(bwt1,
+			//print_node(substring1->normal);
+			//printf("letter added to the left: %c \n", common_alphabet[i]);
+			normal1 = backward_search_interval(bwt1,
 					&substring1->normal, common_alphabet[i], normal1);
 			if (normal1 == NULL) {
+				printf("normal 1 was null\n");
 				continue;
 			}
-			interval* normal2 = backward_search_interval(bwt2,
+			//print_node(substring2->normal);
+			normal2 = backward_search_interval(bwt2,
 					&substring2->normal, common_alphabet[i], normal2);
 			if (normal2 == NULL) {
+				printf("normal 2 was null\n");
 				continue;
 			}
-			interval* reverse1 = update_reverse_interval(&substring1->reverse,
-					normal1, alpha_data1->alphabet, alphabet_length1, c_array1,
+			reverse1 = update_reverse_interval(&substring1->reverse,
+					normal1, common_alphabet, common_alphabet_length, c_array1,
 					common_alphabet[i], reverse1);
-			interval* reverse2 = update_reverse_interval(&substring2->reverse,
-					normal2, alpha_data2->alphabet, alphabet_length2, c_array2,
+			reverse2 = update_reverse_interval(&substring2->reverse,
+					normal2, common_alphabet, common_alphabet_length, c_array2,
 					common_alphabet[i], reverse2);
 
 			new_substring1 = create_substring(normal1, reverse1,
--- src/iterate/maximal_repeats.c
+++ src/iterate/maximal_repeats.c
@@ -43,8 +43,11 @@ int is_interval_left_maximal(interval inter) {
 
 void search_maximal_repeats(substring* node) {
 	if (is_interval_left_maximal(node->normal)) {
-		max_repeat_node max_node = *((max_repeat_node*) malloc(sizeof(max_repeat_node)));;
-		max_node.normal = node->normal;
+		printf("Found maximal repeat.\n");
+		print_node(node);
+		max_repeat_node max_node = *((max_repeat_node*) malloc(sizeof(max_repeat_node)));
+		max_node.normal.i = node->normal.i;
+		max_node.normal.j = node->normal.j;
 		max_node.length = node->length;
 		nodes[nodes_index] = max_node;
 		nodes_index++;
@@ -60,9 +63,10 @@ void print_maximal_repeat_substrings(char* string) {
 	int i;
 
 	for (i = 0; i < nodes_index; i++) {
-		printf("%s \n",
+		printf("String: %s, starting position: %d, length of substring: %d \n", string, nodes[i].normal.i, nodes[i].length);
+		printf("Substring: %s index:%d length: %d \n",
 				substring_from_string(string, nodes[i].normal.i,
-						nodes[i].length));
+						nodes[i].length), nodes[i].normal.i, nodes[i].length);
 	}
 }
 
