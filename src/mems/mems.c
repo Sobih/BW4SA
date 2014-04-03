fc99bf7a68bec8a3e7205050bddc3fea8f6447c8
lassi.vapaakallio@cs.helsinki.fi <Lassi.Vapaakallio@cs.helsinki.fi>
2014-04-03 15:38:10 +0300

    starting mems implementation

--- src/mems/mems.c
+++ src/mems/mems.c
@@ -7,7 +7,26 @@
 
 #include "../../include/structs.h"
 
+static wavelet_tree* mem_bwt1;
+static wavelet_tree* mem_bwt2;
+static wavelet_tree* mem_rbwt1;
+static wavelet_tree* mem_rbwt2;
+static triplet* mems;
+static int mem_triplets_index;
+
+void mum_initialize_bwts(wavelet_tree* bwt1, wavelet_tree* bwt2, wavelet_tree* rbwt1, wavelet_tree* rbwt2) {
+	mem_bwt1 = bwt1;
+	mem_bwt2 = bwt2;
+	mem_rbwt1 = rbwt1;
+	mem_rbwt2 = rbwt2;
+	mems = calloc(100, sizeof(triplet));
+	mem_triplets_index = 0;
+}
+
+
 
 void search_mems(substring* node1, substring* node2) {
 
 }
+
+
