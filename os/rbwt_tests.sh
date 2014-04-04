a3849cd142ea53a9dcea883a0ffae8d75953fefc
Max Sandberg <sandberg.max@gmail.com>
2014-04-04 17:01:57 +0300

    Updated build scripts to use new folder structure

--- os/rbwt_tests.sh
+++ os/rbwt_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc -std=c99 -o src/iterate/rbwt.o src/iterate/rbwt.c src/bwt/s_to_bwt.c -I src/bwt -I libs/DBWT/ libs/DBWT/*.c tests/iterate/check_rbwt.c src/wavelet_tree/wavelet_tree.c src/array/bit_vector.c src/utils.c `pkg-config --cflags --libs check` -lm
+gcc -std=c99 -o src/core/rbwt.o src/core/rbwt.c src/core/s_to_bwt.c -I src/bwt -I libs/DBWT/ libs/DBWT/*.c tests/core/check_rbwt.c src/utils/wavelet_tree.c src/utils/bit_vector.c src/utils/utils.c `pkg-config --cflags --libs check` -lm
 
 
-./src/iterate/rbwt.o
+./src/core/rbwt.o
