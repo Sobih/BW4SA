c4be16643accc7cca91d8ac04bc18618c35541ae
lassi.vapaakallio@cs.helsinki.fi <Lassi.Vapaakallio@cs.helsinki.fi>
2014-04-24 18:50:54 +0300

    max repeats pass all tests, ui and sorter tests not fixed yet

--- tests/applications/check_maximal_repeats.c
+++ tests/applications/check_maximal_repeats.c
@@ -18,163 +18,168 @@
 #include <check.h>
 
 START_TEST(test_maximal_repeat2)
-{
-	parameter_struct* params = initialize_for_max_repeats("abracadabra");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	ck_assert_int_eq(2, results->length);
-	ck_assert_int_eq(1, results->data[0].normal.i);
-	ck_assert_int_eq(5, results->data[0].normal.j);
-	ck_assert_int_eq(1, results->data[0].length);
-	ck_assert_int_eq(2, results->data[1].normal.i);
-	ck_assert_int_eq(3, results->data[1].normal.j);
-	ck_assert_int_eq(4, results->data[1].length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("abracadabra");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		ck_assert_int_eq(2, results->length);
+		ck_assert_int_eq(1, results->data[0].normal.i);
+		ck_assert_int_eq(5, results->data[0].normal.j);
+		ck_assert_int_eq(1, results->data[0].length);
+		ck_assert_int_eq(2, results->data[1].normal.i);
+		ck_assert_int_eq(3, results->data[1].normal.j);
+		ck_assert_int_eq(4, results->data[1].length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 START_TEST(test_maximal_repeat2_mapped)
-{
-	parameter_struct* params = initialize_for_max_repeats("abracadabra");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	max_repeat_with_indexes* nodes = results->data;
-	map_maximal_repeats_to_string(nodes, s_to_bwt("abracadabra"),results->length);
-	ck_assert_int_eq(2, results->length);
-			ck_assert_int_eq(0, nodes[0].indexes[0]);
-			ck_assert_int_eq(1, nodes[0].length);
-			ck_assert_int_eq(0, nodes[1].indexes[0]);
-			ck_assert_int_eq(4, nodes[1].length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("abracadabra");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		max_repeat_node* nodes = results->data;
+		max_repeat_with_indexes* mapped_nodes = map_maximal_repeats_to_string(
+				nodes, s_to_bwt("abracadabra"), results->length,
+				max_repeat_make_bit_vector(results, state));
+		ck_assert_int_eq(2, results->length);
+		ck_assert_int_eq(0, mapped_nodes[0].indexes[0]);
+		ck_assert_int_eq(1, mapped_nodes[0].length);
+		ck_assert_int_eq(0, mapped_nodes[1].indexes[0]);
+		ck_assert_int_eq(4, mapped_nodes[1].length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 START_TEST(test_maximal_repeat2b)
-{
-	parameter_struct* params = initialize_for_max_repeats("hattivatti");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	max_repeat_node* nodes = results->data;
-	ck_assert_int_eq(2, results->length);
-	ck_assert_int_eq(6, nodes[0].normal.i);
-	ck_assert_int_eq(9, nodes[0].normal.j);
-	ck_assert_int_eq(1, nodes[0].length);
-	ck_assert_int_eq(1, nodes[1].normal.i);
-	ck_assert_int_eq(2, nodes[1].normal.j);
-	ck_assert_int_eq(4, nodes[1].length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("hattivatti");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		max_repeat_node* nodes = results->data;
+		ck_assert_int_eq(2, results->length);
+		ck_assert_int_eq(6, nodes[0].normal.i);
+		ck_assert_int_eq(9, nodes[0].normal.j);
+		ck_assert_int_eq(1, nodes[0].length);
+		ck_assert_int_eq(1, nodes[1].normal.i);
+		ck_assert_int_eq(2, nodes[1].normal.j);
+		ck_assert_int_eq(4, nodes[1].length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 START_TEST(test_maximal_repeat2b_mapped)
-{
-	parameter_struct* params = initialize_for_max_repeats("hattivatti");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	max_repeat_with_indexes* nodes = results->data;
-	map_maximal_repeats_to_string(nodes, s_to_bwt("hattivatti"),results->length);
-	ck_assert_int_eq(2, results->length);
-	ck_assert_int_eq(1, nodes[0].indexes[0]);
-	ck_assert_int_eq(4, nodes[0].length);
-	ck_assert_int_eq(2, nodes[1].indexes[0]);
-	ck_assert_int_eq(1, nodes[1].length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("hattivatti");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		max_repeat_with_indexes* nodes = map_maximal_repeats_to_string(
+				results->data, s_to_bwt("hattivatti"), results->length,
+				max_repeat_make_bit_vector(results, state));
+		ck_assert_int_eq(2, results->length);
+		ck_assert_int_eq(1, nodes[0].indexes[0]);
+		ck_assert_int_eq(4, nodes[0].length);
+		ck_assert_int_eq(2, nodes[1].indexes[0]);
+		ck_assert_int_eq(1, nodes[1].length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 START_TEST(test_maximal_repeat3)
-{
-	parameter_struct* params = initialize_for_max_repeats("balalaikka");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	max_repeat_node* nodes = results->data;
-	ck_assert_int_eq(3, results->length);
-	ck_assert_int_eq(1, nodes[0].normal.i);
-	ck_assert_int_eq(4, nodes[0].normal.j);
-	ck_assert_int_eq(1, nodes[0].length);
-	ck_assert_int_eq(7, nodes[1].normal.i);
-	ck_assert_int_eq(8, nodes[1].normal.j);
-	ck_assert_int_eq(1, nodes[1].length);
-	ck_assert_int_eq(3, nodes[2].normal.i);
-	ck_assert_int_eq(4, nodes[2].normal.j);
-	ck_assert_int_eq(3, nodes[2].length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("balalaikka");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		max_repeat_node* nodes = results->data;
+		ck_assert_int_eq(3, results->length);
+		ck_assert_int_eq(1, nodes[0].normal.i);
+		ck_assert_int_eq(4, nodes[0].normal.j);
+		ck_assert_int_eq(1, nodes[0].length);
+		ck_assert_int_eq(7, nodes[1].normal.i);
+		ck_assert_int_eq(8, nodes[1].normal.j);
+		ck_assert_int_eq(1, nodes[1].length);
+		ck_assert_int_eq(3, nodes[2].normal.i);
+		ck_assert_int_eq(4, nodes[2].normal.j);
+		ck_assert_int_eq(3, nodes[2].length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 START_TEST(test_maximal_repeat3_mapped)
-{
-	parameter_struct* params = initialize_for_max_repeats("balalaikka");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	max_repeat_with_indexes* nodes = results->data;
-	map_maximal_repeats_to_string(nodes, s_to_bwt("balalaikka"),results->length);
-	ck_assert_int_eq(3, results->length);
-	ck_assert_int_eq(1, nodes[0].indexes[0]);
-	ck_assert_int_eq(1, nodes[0].length);
-	ck_assert_int_eq(1, nodes[1].indexes[0]);
-	ck_assert_int_eq(3, nodes[1].length);
-	ck_assert_int_eq(7, nodes[2].indexes[0]);
-	ck_assert_int_eq(1, nodes[2].length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("balalaikka");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		max_repeat_with_indexes* nodes = map_maximal_repeats_to_string(
+				results->data, s_to_bwt("balalaikka"), results->length,
+				max_repeat_make_bit_vector(results, state));
+		ck_assert_int_eq(3, results->length);
+		ck_assert_int_eq(1, nodes[0].indexes[0]);
+		ck_assert_int_eq(1, nodes[0].length);
+		ck_assert_int_eq(1, nodes[1].indexes[0]);
+		ck_assert_int_eq(3, nodes[1].length);
+		ck_assert_int_eq(7, nodes[2].indexes[0]);
+		ck_assert_int_eq(1, nodes[2].length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 START_TEST(test_maximal_repeat_empty)
-{
-	parameter_struct* params = initialize_for_max_repeats("abc");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	ck_assert_int_eq(0, results->length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("abc");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		ck_assert_int_eq(0, results->length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 START_TEST(test_maximal_repeat1)
-{
-	parameter_struct* params = initialize_for_max_repeats("ilotalo");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	max_repeat_node* nodes = results->data;
-	ck_assert_int_eq(1, results->length);
-	ck_assert_int_eq(3, nodes[0].normal.i);
-	ck_assert_int_eq(4, nodes[0].normal.j);
-	ck_assert_int_eq(2, nodes[0].length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("ilotalo");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		max_repeat_node* nodes = results->data;
+		ck_assert_int_eq(1, results->length);
+		ck_assert_int_eq(3, nodes[0].normal.i);
+		ck_assert_int_eq(4, nodes[0].normal.j);
+		ck_assert_int_eq(2, nodes[0].length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 START_TEST(test_maximal_repeat1_mapped)
-{
-	parameter_struct* params = initialize_for_max_repeats("ilotalo");
-	iterator_state* state = iterate(params);
-	max_repeat_results* results = (max_repeat_results*) params->ret_data;
-	max_repeat_with_indexes* nodes = results->data;
-	map_maximal_repeats_to_string(nodes, s_to_bwt("ilotalo"),results->length);
-	ck_assert_int_eq(1, results->length);
-	ck_assert_int_eq(1, nodes[0].indexes[0]);
-	ck_assert_int_eq(2, nodes[0].length);
+	{
+		parameter_struct* params = initialize_for_max_repeats("ilotalo");
+		iterator_state* state = iterate(params);
+		max_repeat_results* results = (max_repeat_results*) params->ret_data;
+		max_repeat_with_indexes* nodes = map_maximal_repeats_to_string(
+				results->data, s_to_bwt("ilotalo"), results->length,
+				max_repeat_make_bit_vector(results, state));
+		ck_assert_int_eq(1, results->length);
+		ck_assert_int_eq(1, nodes[0].indexes[0]);
+		ck_assert_int_eq(2, nodes[0].length);
 
-	free(results->data);
-	free_parameter_struct(params);
-	free_iterator_state(state);
-}END_TEST
+		free(results->data);
+		free_parameter_struct(params);
+		free_iterator_state(state);
+	}END_TEST
 
 //returns 0 if the searched substring is not found
 int find_and_remove_test_substr(test_substr* head, int index, int length) {
@@ -195,86 +200,94 @@ int find_and_remove_test_substr(test_substr* head, int index, int length) {
 }
 
 START_TEST(test_max_repeats_randomized)
-{
-	srand(time(NULL));
-	char* test;
-	wavelet_tree* bwt;
-	int* suffix_array;
-	int length;
-	char* alphabet = "abcgdf";
+	{
+		srand(time(NULL));
+		char* test;
+		wavelet_tree* bwt;
+		int* suffix_array;
+		int length;
+		char* alphabet = "abcgdf";
 
-	for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 100; i++) {
 
-		test = generate_random_string(alphabet, rand() % 100 + 1);
-		bwt = s_to_bwt(test);
-		suffix_array = map_create_suffix_array_from_bwt(bwt);
+			test = generate_random_string(alphabet, rand() % 100 + 1);
+			bwt = s_to_bwt(test);
+			suffix_array = map_create_suffix_array_from_bwt(bwt);
 
-		test_substr* maxrep_naive = find_maximal_repeat_substrings(test);
-		parameter_struct* params = initialize_for_max_repeats(test);
-		iterator_state* state = iterate(params);
-		max_repeat_results* results = (max_repeat_results*) params->ret_data;
-		max_repeat_node* maxrep_fast = results->data;
-		int max_number_nodes = results->length;
+			test_substr* maxrep_naive = find_maximal_repeat_substrings(test);
+			parameter_struct* params = initialize_for_max_repeats(test);
+			iterator_state* state = iterate(params);
+			max_repeat_results* results = (max_repeat_results*) params->ret_data;
+			max_repeat_node* maxrep_fast = results->data;
+			int max_number_nodes = results->length;
 
-		for(int j = 0; j < max_number_nodes; j++){
-			for(int k = 0; k + maxrep_fast[j].normal.i <=  maxrep_fast[j].normal.j; k++){
+			for (int j = 0; j < max_number_nodes; j++) {
+				for (int k = 0;
+						k + maxrep_fast[j].normal.i <= maxrep_fast[j].normal.j;
+						k++) {
 
-				fail_unless(find_and_remove_test_substr(maxrep_naive,
-						 suffix_array[maxrep_fast[j].normal.i + k], maxrep_fast[j].length));
+					fail_unless(
+							find_and_remove_test_substr(maxrep_naive,
+									suffix_array[maxrep_fast[j].normal.i + k],
+									maxrep_fast[j].length));
+				}
 			}
-		}
-		print_substring_list(test, maxrep_naive);
-		fail_unless(maxrep_naive->next == NULL);
+			print_substring_list(test, maxrep_naive);
+			fail_unless(maxrep_naive->next == NULL);
 
-		free_wavelet_tree(bwt);
-		free(suffix_array);
-		free(maxrep_naive);
-		free(results->data);
-		free_parameter_struct(params);
-		free_iterator_state(state);
-	}
-}END_TEST
+			free_wavelet_tree(bwt);
+			free(suffix_array);
+			free(maxrep_naive);
+			free(results->data);
+			free_parameter_struct(params);
+			free_iterator_state(state);
+		}
+	}END_TEST
 
 START_TEST(test_max_repeats_randomized2)
-{
-	srand(time(NULL));
-	char* test;
-	wavelet_tree* bwt;
-	int* suffix_array;
-	int length;
-	char* alphabet = "aoskfdhebs";
+	{
+		srand(time(NULL));
+		char* test;
+		wavelet_tree* bwt;
+		int* suffix_array;
+		int length;
+		char* alphabet = "aoskfdhebs";
 
-	for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 
-		test = generate_random_string(alphabet, rand() % 1000 + 100);
-		bwt = s_to_bwt(test);
-		suffix_array = map_create_suffix_array_from_bwt(bwt);
+			test = generate_random_string(alphabet, rand() % 1000 + 100);
+			bwt = s_to_bwt(test);
+			suffix_array = map_create_suffix_array_from_bwt(bwt);
 
-		test_substr* maxrep_naive = find_maximal_repeat_substrings(test);
-		parameter_struct* params = initialize_for_max_repeats(test);
-		iterator_state* state = iterate(params);
-		max_repeat_results* results = (max_repeat_results*) params->ret_data;
-		max_repeat_node* maxrep_fast = results->data;
-		int max_number_nodes = results->length;
+			test_substr* maxrep_naive = find_maximal_repeat_substrings(test);
+			parameter_struct* params = initialize_for_max_repeats(test);
+			iterator_state* state = iterate(params);
+			max_repeat_results* results = (max_repeat_results*) params->ret_data;
+			max_repeat_node* maxrep_fast = results->data;
+			int max_number_nodes = results->length;
 
-		for(int j = 0; j < max_number_nodes; j++){
-			for(int k = 0; k + maxrep_fast[j].normal.i <=  maxrep_fast[j].normal.j; k++){
+			for (int j = 0; j < max_number_nodes; j++) {
+				for (int k = 0;
+						k + maxrep_fast[j].normal.i <= maxrep_fast[j].normal.j;
+						k++) {
 
-				fail_unless(find_and_remove_test_substr(maxrep_naive,
-						 suffix_array[maxrep_fast[j].normal.i + k], maxrep_fast[j].length));
+					fail_unless(
+							find_and_remove_test_substr(maxrep_naive,
+									suffix_array[maxrep_fast[j].normal.i + k],
+									maxrep_fast[j].length));
+				}
 			}
-		}
-		print_substring_list(test, maxrep_naive);
-		fail_unless(maxrep_naive->next == NULL);
+			print_substring_list(test, maxrep_naive);
+			fail_unless(maxrep_naive->next == NULL);
 
-		free_wavelet_tree(bwt);
-		free(suffix_array);
-		free(maxrep_naive);
-		free(results->data);
-		free_parameter_struct(params);
-		free_iterator_state(state);
-	}
-}END_TEST
+			free_wavelet_tree(bwt);
+			free(suffix_array);
+			free(maxrep_naive);
+			free(results->data);
+			free_parameter_struct(params);
+			free_iterator_state(state);
+		}
+	}END_TEST
 
 TCase * create_max_repeats_randomized_test_case(void) {
 	TCase * tc_randrep = tcase_create("max_repeats_randomized");
